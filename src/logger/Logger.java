aafb957b45ffaf38573b3df16a52585a838cddaa
kristofgyori <gykristof0206@gmail.com>
2018-03-15 13:37:28 +0100

    Insert tab when new seq. starting function implementation.

--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -8,7 +8,7 @@ import game.*;
 
 public class Logger {
 	private static final Map<Object, String> logData;
-
+	private static int tabNum = 0;
 	static {
 		logData = new HashMap<Object, String>();
 	}
@@ -21,10 +21,20 @@ public class Logger {
 		logData.put(obj, str);
 	}
 	public static void enter(Object o, String fv) {	
-		System.out.println("->" + logData.get(o) + "." + fv + ":"); 
+		System.out.println(getTabs() + "->" + logData.get(o) + "." + fv + ":");
+		tabNum++;
 	}
 	public static void  exit(Object o, String fv) {	
-		System.out.println("<-"+ logData.get(o) + "." + fv);
+		System.out.println(getTabs() + "<-"+ logData.get(o) + "." + fv);
+		tabNum--;
+	}
+	
+	private static String getTabs() {
+		String tabs = "";
+		for (int i = 1; i<=tabNum; i++) {
+			tabs = tabs.concat("\t");
+		}
+		return tabs;
 	}
 
 }
