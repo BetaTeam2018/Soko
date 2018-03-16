55e300ec1585ea623dcfde46fc5b867831822d72
kristofgyori <gykristof0206@gmail.com>
2018-03-17 00:05:41 +0100

    Logger jav1

--- src/logger/Logger.java
+++ src/logger/Logger.java
@@ -7,19 +7,53 @@ import game.*;
 
 
 public class Logger {
+	/**
+	 * A naplózni kívánt objektumokat a sztring megfelelõjével együtt tárolja kulcs-érték párban.  
+	 */
 	private static final Map<Object, String> logData;
+	/**
+	 * A szükséges tabulátorok száma. 
+	 */
 	private static int tabNum = 0;
+	
 	static {
 		logData = new HashMap<Object, String>();
 	}
+	
+	
 	/**
-	 * 
-	 * @param o key
-	 * @param s value
+	 * Beszúr egy kulcs-érték párt a logData HashMap attribútumba. 
+	 * @param key kulcs
+	 * @param value érték 
 	 */
-	public static void putLogData(Object obj, String str) {
-		logData.put(obj, str);
+	public static void putLogData(Object key, String value) {
+		logData.put(key, value);
 	}
+	
+	/**
+	 * Visszadja az adott kulcshoz tarozó értéket a logData tárolóból. 
+	 */
+	public static String getLogDataString(Object key) {
+		return logData.get(key);
+	}
+	
+	/**
+	 * A függvény paramétereinek sztring elõállítására szolgál. 
+	 * Az adott objektumlistából elõállítja az objektumok sztring megfelelõit felsoroló sztringet. 
+	 * @param params objektumok tömbje, amiben a objektumreferenciák vannak.
+	 * @return Az elõállított paraméterlista sztring formátumban.
+	 */
+	public static String objParamListString(Object[] params) {
+		String strParams = "";
+		int i = 0;
+		for (Object key : params) {
+			i++;
+			strParams += logData.get(key);
+			if(params.length != i) strParams += ", ";
+		}
+		return strParams;
+	}
+	
 	public static void enter(Object o, String fv) {	
 		System.out.println(getTabs() + "-> " + logData.get(o) + "." + fv + ":");
 		tabNum++;
